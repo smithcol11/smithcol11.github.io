7e3594fbd24c23d412185da81892b6e5c86026c0
Colin Smith <smithcol@pdx.edu>
2022-11-21 09:22:51 -0800

    update routes, no more lazy loading.

--- src/router/route.js
+++ src/router/route.js
@@ -4,6 +4,11 @@ import {
   createMemoryHistory,
   createWebHashHistory,
 } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import WorkHistory from "../views/WorkHistory.vue";
+import Projects from "../views/Projects.vue";
+import Contact from "../views/Contact.vue";
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,27 +20,27 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/Home.vue"),
+      component: Home,
     },
     {
       path: "/about",
       name: "about",
-      component: () => import("../views/About.vue"),
+      component: About,
     },
     {
       path: "/workHistory",
       name: "workHistory",
-      component: () => import("../views/WorkHistory.vue"),
+      component: WorkHistory,
     },
     {
       path: "/projects",
       name: "projects",
-      component: () => import("../views/Projects.vue"),
+      component: Projects,
     },
     {
       path: "/contact",
       name: "contact",
-      component: () => import("../views/Contact.vue"),
+      component: Contact,
     },
   ],
 });
