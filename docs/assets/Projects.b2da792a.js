f5a1f4fa78da912ed6be7146b93d9a8150d22d19
Colin Smith <smithcol@pdx.edu>
2022-11-21 08:24:45 -0800

    Router to use history mode

---
+++ docs/assets/Projects.b2da792a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as s,a as t}from"./index.33eeeddc.js";const c={},r={class:"pageView"},_=t("p",null,"projects",-1),a=[_];function n(i,d){return o(),s("div",r,a)}const l=e(c,[["render",n]]);export{l as default};
--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
+  mode: "history",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
